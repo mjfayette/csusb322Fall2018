72ba891108c65ead011fe6be07427bc9e573e568
mjfayette <44035236+mjfayette@users.noreply.github.com>
2018-10-31 12:00:42 -0700

    Update end.js

--- end.js
+++ end.js
@@ -2,10 +2,10 @@ document.body.addEventListener("keydown", function (e) {
     keys[e.keyCode] = true;
 });
 
-body.addEventListener("keyup", function (e) {
+document.body.addEventListener("keyup", function (e) {
     keys[e.keyCode] = false;
 });
 
-window.addEventListener("load", function () {
+addEventListener("load", function () {
     update();
 });
